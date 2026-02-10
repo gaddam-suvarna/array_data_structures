3e358ae4a09c310a18211951ee0af75421b142c2
gaddam-suvarna <suvarnagaddam504@gmail.com>
2026-02-11 00:34:56 +0530

    Update AlternateElements.java

--- MyArraysDSAlgosProject/src/main/java/AlternateElements.java
+++ MyArraysDSAlgosProject/src/main/java/AlternateElements.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 public class AlternateElements {
 
     public  static void main(String[] args) {
-
+        //Enter the array size
         System.out.println("Enter the size");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int array[] = new int[n];
-
+        //Enter the array elements
         System.out.println("Enter the elements");
         for(int i = 0; i < n ; i ++) {
             array[i] = sc.nextInt();
         }
-
+        //print the alternate elements
         System.out.println("Alternate elements are");
         for(int i = 0; i < n ; i += 2) {
             System.out.println(array[i]);
@@ -24,12 +24,7 @@ public class AlternateElements {
 
     }
 
-
-
-
-
-
-
+//Static allocation
      /*static int arr[] = {10,20,30,40,50};
     // Iterate over all alternate elements
     public static void main(String[] args) {
